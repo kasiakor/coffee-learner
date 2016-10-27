5d2ec4d68705fe5ba59e749c17190ad45870783c
Kasia Kordys <kasia.kordys@gmail.com>
2016-10-28 00:20:12 +0100

    Add coffee creator

---
+++ scripts/createCoffee.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$('button').click(function() {
+		var coffee = $('#coffee').val();
+		var milk =$('#milk').val();
+		var result = $('#game-result');
+		
+		if(coffee === "1" && milk === "0" ) {
+			result.val("Espresso");
+		}
+		else if (coffee ==="2" && milk === "0") {
+			result.val("Double espresso");
+		}
+		else if (coffee === "1" && milk === "1") {
+			 result.val("Cortado");
+		}
+		else if (coffee === "1" && milk === "2") {
+			 result.val("Caffe Latte");
+		}
+		else if (coffee === "2" && milk === "1") {
+			 result.val("Cappuccino");
+		}
+		else if (coffee === "2" && milk === "2") {
+			 result.val("Strong Caffe Latte :)");
+		}
+  	});
+});
