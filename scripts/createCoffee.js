36824480594292aa9c415d1c9fda4eba2d3a8f03
Kasia Kordys <kasia.kordys@gmail.com>
2016-10-31 21:56:42 +0000

    Add js functionality to load images

--- scripts/createCoffee.js
+++ scripts/createCoffee.js
@@ -5,22 +5,28 @@ $(document).ready(function() {
 		var result = $('#game-result');
 		
 		if(coffee === "1" && milk === "0" ) {
-			result.val("Espresso");
-		}
-		else if (coffee ==="2" && milk === "0") {
-			result.val("Double espresso");
-		}
-		else if (coffee === "1" && milk === "1") {
-			 result.val("Cortado");
-		}
-		else if (coffee === "1" && milk === "2") {
-			 result.val("Caffe Latte");
-		}
-		else if (coffee === "2" && milk === "1") {
-			 result.val("Cappuccino");
-		}
-		else if (coffee === "2" && milk === "2") {
-			 result.val("Strong Caffe Latte :)");
-		}
+  			result.val("Espresso");
+  			$("#coffee-image").html("<img src='Images/espresso-result.jpg'"+"alt='description' />");
+  		}
+  		else if (coffee ==="2" && milk === "0") {
+  			result.val("Double espresso");
+  			$("#coffee-image").html("<img src='Images/espresso-result.jpg'"+"alt='description' />");
+  		}
+  		else if (coffee === "1" && milk === "1") {
+  			 result.val("Cortado");
+  			 $("#coffee-image").html("<img src='Images/cortado-result.jpg'"+"alt='description' />");		 
+  		}
+  		else if (coffee === "1" && milk === "2") {
+  			 result.val("Caffe Latte");
+  			 $("#coffee-image").html("<img src='Images/caffelatte-result.jpg'"+"alt='description' />");
+  		}
+  		else if (coffee === "2" && milk === "1") {
+  			 result.val("Cappuccino");
+  			 $("#coffee-image").html("<img src='Images/cappuccino-result.jpg'"+"alt='description' />");
+  		}
+  		else if (coffee === "2" && milk === "2") {
+  			 result.val("Strong Caffe Latte");
+  			 $("#coffee-image").html("<img src='Images/caffelatte-result.jpg'"+"alt='description' />");
+  		}
   	});
 });
