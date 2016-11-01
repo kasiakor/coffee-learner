a91dd1cfddef8a83447b0c3e49c7e80794c26903
Kasia Kordys <kasia.kordys@gmail.com>
2016-11-01 23:09:26 +0000

    Improve createCoffee js code

--- scripts/createCoffee.js
+++ scripts/createCoffee.js
@@ -2,31 +2,32 @@ $(document).ready(function() {
 	$('#game button').click(function() {
 		var coffee = $('#coffee').val();
 		var milk =$('#milk').val();
-		var result = $('#game-result');
+		var gameResult = $('#game-result');
+		var coffeeImage = $("#coffee-image");
 		
 		if(coffee === "1" && milk === "0" ) {
-  			result.val("Espresso");
-  			$("#coffee-image").html("<img src='Images/espresso-result.jpg'"+"alt='description' />");
-  		}
-  		else if (coffee ==="2" && milk === "0") {
-  			result.val("Double espresso");
-  			$("#coffee-image").html("<img src='Images/espresso-result.jpg'"+"alt='description' />");
-  		}
-  		else if (coffee === "1" && milk === "1") {
-  			 result.val("Cortado");
-  			 $("#coffee-image").html("<img src='Images/cortado-result.jpg'"+"alt='description' />");		 
-  		}
-  		else if (coffee === "1" && milk === "2") {
-  			 result.val("Caffè Latte");
-  			 $("#coffee-image").html("<img src='Images/caffelatte-result.jpg'"+"alt='description' />");
-  		}
-  		else if (coffee === "2" && milk === "1") {
-  			 result.val("Cappuccino");
-  			 $("#coffee-image").html("<img src='Images/cappuccino-result.jpg'"+"alt='description' />");
-  		}
-  		else if (coffee === "2" && milk === "2") {
-  			 result.val("Strong Caffè Latte");
-  			 $("#coffee-image").html("<img src='Images/caffelatte-result.jpg'"+"alt='description' />");
-  		}
-  	});
+			gameResult.val("Espresso");
+			coffeeImage.html("<img src='Images/espresso-result.jpg' />");
+		}
+		else if (coffee ==="2" && milk === "0") {
+			gameResult.val("Double espresso");
+			coffeeImage.html("<img src='Images/espresso-result.jpg' />");
+		}
+		else if (coffee === "1" && milk === "1") {
+			 gameResult.val("Cortado");
+			 coffeeImage.html("<img src='Images/cortado-result.jpg' />");
+		}
+		else if (coffee === "1" && milk === "2") {
+			 gameResult.val("Caffè Latte");
+			 coffeeImage.html("<img src='Images/caffelatte-result.jpg' />");
+		}
+		else if (coffee === "2" && milk === "1") {
+			 gameResult.val("Cappuccino");
+			 coffeeImage.html("<img src='Images/cappuccino-result.jpg' />");
+		}
+		else if (coffee === "2" && milk === "2") {
+			 gameResult.val("Strong Caffè Latte");
+			 coffeeImage.html("<img src='Images/caffelatte-result.jpg' />");
+		}
+	});
 });
